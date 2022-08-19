73f14a8f43e7c39021a382d58fe0fa0a349f7a90
Durjoy-dcp <unknownccxv@gmail.com>
2022-08-19 07:35:24 +0600

    smart function

--- js/interactive.js
+++ js/interactive.js
@@ -7,15 +7,29 @@ function interactiveNumber(inputId, UpOrDown) {
         inputFieldInt++;
     } else {
         inputFieldInt--;
-        if (inputFieldInt == -1) return;
+        if (inputFieldInt == -1) return 0;
     }
     inputField.value = inputFieldInt;
+    return inputFieldInt;
 }
-document.getElementById('case-btn-plus').addEventListener('click', function () {
-    interactiveNumber('case-input-field', true);
+function interactivePrice(inputId, price) {
+    document.getElementById(inputId).innerText = price;
+}
 
-})
-document.getElementById('case-btn-minus').addEventListener('click', function () {
-    interactiveNumber('case-input-field', false);
+function domEventListener(inputId, inputField, price, priceShow, UpOrDown) {
+    document.getElementById(inputId).addEventListener('click', function () {
+        let howMany = interactiveNumber(inputField, UpOrDown);
+        interactivePrice(priceShow, howMany * price);
 
-})
+    })
+}
+domEventListener('phone-btn-plus', 'phone-input-field', 1219, 'phone-price', true);
+domEventListener('phone-btn-minus', 'phone-input-field', 1219, 'phone-price', false);
+domEventListener('case-btn-plus', 'case-input-field', 59, 'case-price', true);
+domEventListener('case-btn-minus', 'case-input-field', 59, 'case-price', false);
+
+// document.getElementById('case-btn-plus').addEventListener('click', function () {
+//     let howMany = interactiveNumber('case-input-field', true);
+//     interactivePrice('case-price', howMany * 59);
+
+// })
